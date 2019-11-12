eb35e52481738d86616985cf170c93919612e7af
lixiangmin <229078506@qq.com>
2019-11-12 15:41:05 +0800

    animation recommend 后端项目初始化，数据表结构初定

---
+++ animation_recommend_backend/src/main/java/com/animation_recommend/animation_recommend_backend/AnimationRecommendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.animation_recommend.animation_recommend_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimationRecommendBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimationRecommendBackendApplication.class, args);
+    }
+
+}
---
+++ animation_recommend_backend/src/test/java/com/animation_recommend/animation_recommend_backend/AnimationRecommendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animation_recommend.animation_recommend_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimationRecommendBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
