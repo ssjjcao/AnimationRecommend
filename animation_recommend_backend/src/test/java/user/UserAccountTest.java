7e46565791fc51bceda68dfb5bdc06bd4ff3f16e
ssjjcao <jjcao16@fudan.edu.cn>
2019-11-26 15:17:21 +0800

    添加用户登录测试

---
+++ animation_recommend_backend/src/test/java/user/UserAccountTest.java
@@ -0,0 +1,46 @@
+package user;
+
+import animation_recommend_backend.AnimationRecommendBackendApplication;
+import animation_recommend_backend.entity.ResponseBox;
+import animation_recommend_backend.entity.User;
+import animation_recommend_backend.service.UserService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AnimationRecommendBackendApplication.class)
+public class UserAccountTest {
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void testUserSignInFailure() {
+        User user = getUser("Dream", "123456");
+        ResponseBox responseBox = userService.signIn(user);
+        Assertions.assertFalse(responseBox.isResult());
+
+        user = getUser("dream", "niubility");
+        responseBox = userService.signIn(user);
+        Assertions.assertFalse(responseBox.isResult());
+
+        //todo :更多的测试用例
+    }
+
+    @Test
+    public void testUserSignInSuccess() {
+        User user = getUser("dream", "123456");
+        ResponseBox responseBox = userService.signIn(user);
+        Assertions.assertTrue(responseBox.isResult());
+
+        user = getUser("bear", "niubility");
+        responseBox = userService.signIn(user);
+        Assertions.assertTrue(responseBox.isResult());
+    }
+
+    private User getUser(String userName, String password) {
+        User user = new User();
+        user.setName(userName);
+        user.setPassword(password);
+        return user;
+    }
+}
