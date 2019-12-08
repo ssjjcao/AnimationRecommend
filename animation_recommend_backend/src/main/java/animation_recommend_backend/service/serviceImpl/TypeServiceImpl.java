f27cad2f8db497f368318f2f43903463092062ab
ssjjcao <jjcao16@fudan.edu.cn>
2019-12-08 19:01:52 +0800

    推荐排序

--- animation_recommend_backend/src/main/java/animation_recommend_backend/service/serviceImpl/TypeServiceImpl.java
+++ animation_recommend_backend/src/main/java/animation_recommend_backend/service/serviceImpl/TypeServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class TypeServiceImpl implements TypeService {
@@ -16,6 +18,18 @@ public class TypeServiceImpl implements TypeService {
 
     @Override
     public List<Animation> getAnimationByType(String typeName) {
-        return new ArrayList<>(typeRepository.getTypeByName(typeName).getAnimations());
+        Set<Animation> animationSet = typeRepository.getTypeByName(typeName).getAnimations();
+        ArrayList<Animation> animations = new ArrayList<>(animationSet);
+        Comparator<Animation> comparator = (o1, o2) -> {
+            int recommendIndex1 = o1.getLike_num() + o1.getPage_view() + o1.getComments().size() - o1.getUnlike_num();
+            int recommendIndex2 = o2.getLike_num() + o2.getPage_view() + o2.getComments().size() - o2.getUnlike_num();
+            if (recommendIndex1 == recommendIndex2) {
+                return 0;
+            } else {
+                return recommendIndex1 > recommendIndex2 ? 1 : -1;
+            }
+        };
+        animations.sort(comparator);
+        return animations;
     }
 }
